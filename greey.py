17fdcecbb7c7b821e67f0c8366caa383f985973e
Trelay Wang <trelwan@celestica.com>
2018-07-29 11:54:30 +0000

    first commit

---
+++ greey.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import requests
+url = "http://finance.sina.com.cn/realstock/company/sz000651/qianfuquan.js?d=2014-06-16"
+r = requests.get(url)
+text = r.text.split("/*")[0].split()[0]
+import demjson
+data =demjson.decode(text)
+from pandas import Series
+obj =Series(data[0]['data'])
+print obj.index
+
