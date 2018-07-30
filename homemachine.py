e8c51fec2b97a092787bd78e217d7b6f43719176
Trelay Wang <trelwan@celestica.com>
2018-07-30 13:23:06 +0000

    add home machine

---
+++ homemachine.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#coding=utf-8
+import tushare as ts
+ts.get_industry_classified()
+data_all = ts.get_industry_classified()
+data_home = data_all[data_all["c_name"]=="家电行业"]
+print data_home
