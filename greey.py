f3796e559845b01f028bb484ce6b9113934004f1
Trelay wang <trelwan@celestica.com>
2018-07-31 20:24:21 +0800

    change the type of index/value

--- greey.py
+++ greey.py
@@ -6,6 +6,18 @@ text = r.text.split("/*")[0].strip()
 import demjson
 data =demjson.decode(text)
 from pandas import Series
+import pandas as pd
+from pandas import DataFrame
 obj =Series(data[0]['data'])
-print obj.index
+obj1=pd.to_numeric(obj)
+obj1.index=pd.to_datetime(obj1.index,format="_%Y_%m_%d")
+#from pandas import DataFrame
+#data1=DataFrame(obj1)
+#data1.columns=["Price",]
+#print(data1)
+#data1.Price[data1.index.min()]
+#data1.Price[data1.index.max()]
 
+print(obj1)
+rate = obj1[obj1.index.max()]/obj1[obj1.index.min()]
+print(rate)
