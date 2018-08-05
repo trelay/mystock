2a2718f398e9c5a4c3b8f644f5822ef3095a99dc
Trelay wang <trelwan@celestica.com>
2018-08-05 14:18:38 +0000

    add except

--- get_all_perf.py
+++ get_all_perf.py
@@ -13,7 +13,6 @@ perf = Series()
 ts.get_industry_classified()
 data_all = ts.get_industry_classified()
 #data_home = data_all[data_all["c_name"]=="家电行业"]
-perf["total"] = data_all.count()['code']
 
 def get_rate(code):
     url = "http://finance.sina.com.cn/realstock/company/sz{0}/qianfuquan.js?d=2014-06-16".format(code)
@@ -23,17 +22,20 @@ def get_rate(code):
         r = requests.get(url)
     if r.status_code!=200:
        print("can not find this share")
-       return
+       return code,"NAA"
     text = r.text.split("/*")[0].strip()
     data =demjson.decode(text)
     obj =Series(data[0]['data'])
     obj1=pd.to_numeric(obj)
-    obj1.index=pd.to_datetime(obj1.index,format="_%Y_%m_%d")
-    rate = obj1[obj1.index.max()]/obj1[obj1.index.min()]
-    print "code:{}".format(code)
-    print "rate:{}".format(rate)
+    try: 
+        obj1.index=pd.to_datetime(obj1.index,format="_%Y_%m_%d")
+        rate = obj1[obj1.index.max()]/obj1[obj1.index.min()]
+    except:
+        rate = "NAN"
+    print "{}:{}".format(code,rate)
     return code, rate
 for com in numpy.array(data_all.code):
-    code, rate = get_rate(com)
-    perf[code]= rate
-    perf.to_csv('perf.csv')
+    if not com in done.index:
+        code, rate = get_rate(com)
+        perf[code]= rate
+        perf.to_csv('perf_new.csv')
